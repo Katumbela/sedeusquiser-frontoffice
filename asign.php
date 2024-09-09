bdb92542cbdac930ab6731cd2aec67a60baaa417
Joao Afonso Katombela <ja3328173@gmail.com>
2024-09-09 10:55:54 +0100

    feat: asign.php. Criação de novo arquivo

---
+++ asign.php
@@ -0,0 +1,484 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-FHBP9WB24G"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-FHBP9WB24G');
+</script>
+
+
+	<script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-1921695646246423" crossorigin="anonymous">
+	</script>
+
+
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Assinar Pacotes</title>
+    <link rel="shortcut icon" href="img/logo_p.png" type="image/x-icon">
+    <link rel="stylesheet" href="estilo.css">
+    <link rel="stylesheet" href="carousel.css">
+    <link rel="stylesheet" href="aos/aos.css">
+    <link rel="stylesheet" href="owl/dist/assets/owl.carousel.css">
+    <link rel="stylesheet" href="owl/dist/assets/owl.theme.default.css">
+    <link rel="stylesheet" href="estilo.beneficios.css">
+    <link rel="stylesheet" href="produto.css">
+    <link rel="stylesheet" href="cursos.css">
+    <script src="js/jquery.js"></script>
+    <script src="js/jquery.min.js"></script>
+    <script src="owl/dist/owl.carousel.js"></script>
+    <script src="aos/aos.js"></script>
+    <script src="js/bootstrap.js"></script>
+    <link href="css/bootstrap.css" rel="stylesheet">
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+</head>
+<style>
+    
+    @font-face {
+        font-family: euro;
+        src: url(fontes/eurof75.ttf);
+    }
+  ::-webkit-scrollbar{
+        width: 15px!important;
+        cursor: pointer!important;
+    }
+
+    ::-webkit-scrollbar-track{
+        box-shadow: inset 0 0 5px grey!important;
+        border-radius: 10px!important;
+    }
+
+    ::-webkit-scrollbar-thumb{
+        background: #de0877!important;
+        border-radius: 10px!important;
+    }
+
+    ::-webkit-scrollbar-thumb:hover{
+        background: #f787c1!important;
+    }
+
+
+    @font-face {
+        font-family: euro2;
+        src: url(fontes/eurof55.ttf);
+    }
+
+    @font-face {
+        font-family: gokside;
+        src: url(fontes/78936___.TTF);
+    }
+
+    h2{
+        font-family: 'euro';
+        font-weight: bolder;
+    }
+
+    h4{
+        font-family: euro2;
+    }
+
+    body{
+    font-family: gokside;
+    }
+
+    .form-control:focus{
+        box-shadow: 0px 0px 4px 1px #de0877!important;
+    }
+
+@media screen and (max-width: 800px) {
+    .img{
+        position: fixed;
+        top: 0;
+        left: 0;
+        right: 0;
+        height: 15vh;
+        z-index: 10000;
+    }
+
+    .pcs{
+        margin-top: 15vh;
+    }
+}
+
+
+.f-ac2{
+    font-family: achen1;
+  }
+
+  .f-caprica{
+    font-family: caprica;
+  }
+
+  .f-t{
+    font-family: caprica;
+  }
+
+  .f-acid{
+    font-family: acid;
+  }
+
+
+  .f-edoz{
+    font-family: edoz;
+  }
+
+
+  .f-euro2{
+    font-family: euro2;
+  }
+
+  .f-euro{
+    font-family: euro;
+  }
+
+
+  .f-achen{
+    font-family: achen;
+  }
+
+  .f-cut{
+    font-family: cut;
+  }
+
+  .f-euro{
+    font-family: euro2!important;
+  }
+
+  .outros{
+    background: url(img/fundo.jpg) center center;
+  }
+
+
+
+    @media screen and (min-width: 1000px) {
+        .img{
+            position: fixed;
+            top: 0;
+            left: 3rem;
+            right: 3rem;
+            height: 15vh;
+            z-index: 10000;
+        }
+
+        .pcs{
+            margin-top: 15vh;
+        }
+    }
+
+    
+
+  .curso_conteudo h5{
+    border-left: 3px solid #de0877;
+    border-radius: 6px;
+    padding: .2rem 1rem;
+    background: linear-gradient(90deg, #de0877,transparent, transparent, transparent, transparent, transparent, transparent, transparent, transparent, transparent, #de0877);
+  }
+
+  
+@media screen and (min-width:1200px) {
+.o_quadro > div{
+  width: 90%!important;
+}
+
+body{
+  margin: 0!important;
+  background: #ffe6f3;
+  padding: 0!important;
+}
+  
+}
+  
+@font-face {
+        font-family: euro;
+        src: url(fontes/eurof75.ttf);
+    }
+
+    @font-face {
+        font-family: euro2;
+        src: url(fontes/eurof55.ttf);
+    }
+
+    @font-face {
+        font-family: achen;
+        src: url(fontes/AACHEN_B.TTF);
+    }
+
+    @font-face {
+        font-family: gokside;
+        src: url(fontes/78936___.TTF);
+    }
+
+    @font-face {
+        font-family: caprica;
+        src: url(fontes/CapricaScriptPersonalUse.ttf)
+    }
+
+
+    @font-face {
+        font-family: edoz;
+        src: url(fontes/edosz.ttf)
+    }
+
+
+    @font-face {
+        font-family: cut;
+        src: url(fontes/A\ Cut\ Above\ The\ Rest.ttf)
+    }
+
+
+    @font-face {
+        font-family: acid;
+        src: url(fontes/Acidic.TTF)
+    }
+
+
+
+    @font-face {
+        font-family: achen1;
+        src: url(fontes/Aachenb.ttf)
+    }
+
+
+  .text-start{
+    text-align: start!important;
+  }
+
+  html{
+    overflow-x: hidden!important;
+  }
+
+  .foo ul {
+    list-style-type: none;
+    margin-left: -1rem;
+    margin-top: 1rem;
+  }
+
+  .foo ul li a{
+    color: white;
+  }
+
+  .curso_conteudo h5{
+    border-left: 3px solid #de0877;
+    border-radius: 6px;
+    padding: .2rem 1rem;
+    background: linear-gradient(90deg, #de0877,transparent, transparent, transparent, transparent, transparent, transparent, transparent, transparent, transparent, #de0877);
+  }
+
+  body{
+    overflow-x: hidden!important;
+  }
+
+
+</style>
+<body class="bg-light">
+    <div class="container">
+    <div class="img bg-light">
+        <div class="text-start px-3">
+            <a href="index.html">
+                <svg class="m-1" version="1.1" fill="#de0877" xmlns="http://www.w3.org/2000/svg" width="34" height="34" viewBox="0 0 24 24">
+            <title>Voltar</title>
+            <path d="M23.312 11.25h-19.558l4.5-4.5-1.061-1.061-6.31 6.31 6.31 6.31 1.061-1.061-4.5-4.5h19.558v-1.5z"></path>
+            </svg>
+        </a>   
+        </div>
+    <center>
+        <img src="img/logo_s.png" height="50em" alt="">
+    </center>
+    <hr style="margin: 1rem 1rem;">
+  </div>  
+<br><br>
+<script async="async" data-cfasync="false" src="//pl20518975.highcpmrevenuegate.com/70d0edb4bd3cc093d6bc011f1d7c4120/invoke.js"></script>
+<div id="container-70d0edb4bd3cc093d6bc011f1d7c4120"></div>
+<center>
+  <h1 class="">Escolha o pacote que deseja aderir</h1>
+  
+
+   
+<div class="owl-carousel owl-theme container ">
+    <div class="item text-start" aria-disabled="true">
+        <div class="quadro_curso shadow">
+          <div class="card-footer c1">
+          <div class="curso_conteudo text-start">
+            <h5 class="text-s f-t">Administrativo</h5>
+              <p class="f-13 text-start">
+            <span><span class="text-dark def text-start"><svg version="1.1" xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" fill="green" viewBox="0 0 24 24">
+                  <title>incluido</title>
+                  <path d="M9.331 21.375l-0.346-0.352-0.185-0.183-4.040-4.041 0.002-0.003-3.922-3.922 4.576-4.576 3.917 3.917 9.341-9.341 4.576 4.576-9.869 9.874zM6.872 16.79l2.459 2.459 0.005-0.005 0.003 0.003 2.452-2.452 0.528-0.533 8.811-8.811-2.454-2.454-9.341 9.341-3.917-3.917-2.454 2.454 3.912 3.912z"></path>
+                  </svg> Até 3 cursos </span><br>
+            <span><span class="text-dark def"><svg version="1.1" xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" fill="green" viewBox="0 0 24 24">
+                  <title>incluido</title>
+                  <path d="M9.331 21.375l-0.346-0.352-0.185-0.183-4.040-4.041 0.002-0.003-3.922-3.922 4.576-4.576 3.917 3.917 9.341-9.341 4.576 4.576-9.869 9.874zM6.872 16.79l2.459 2.459 0.005-0.005 0.003 0.003 2.452-2.452 0.528-0.533 8.811-8.811-2.454-2.454-9.341 9.341-3.917-3.917-2.454 2.454 3.912 3.912z"></path>
+                  </svg> Centro escolhido</span><br>
+            <span><span class="text-dark def"><svg version="1.1" xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" fill="green" viewBox="0 0 24 24">
+                  <title>incluido</title>
+                  <path d="M9.331 21.375l-0.346-0.352-0.185-0.183-4.040-4.041 0.002-0.003-3.922-3.922 4.576-4.576 3.917 3.917 9.341-9.341 4.576 4.576-9.869 9.874zM6.872 16.79l2.459 2.459 0.005-0.005 0.003 0.003 2.452-2.452 0.528-0.533 8.811-8.811-2.454-2.454-9.341 9.341-3.917-3.917-2.454 2.454 3.912 3.912z"></path>
+                  </svg> Certificado</span><br>
+            <span><span class="text-dark def"><svg version="1.1" xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" fill="green" viewBox="0 0 24 24">
+                  <title>incluido</title>
+                  <path d="M9.331 21.375l-0.346-0.352-0.185-0.183-4.040-4.041 0.002-0.003-3.922-3.922 4.576-4.576 3.917 3.917 9.341-9.341 4.576 4.576-9.869 9.874zM6.872 16.79l2.459 2.459 0.005-0.005 0.003 0.003 2.452-2.452 0.528-0.533 8.811-8.811-2.454-2.454-9.341 9.341-3.917-3.917-2.454 2.454 3.912 3.912z"></path>
+                  </svg> Até 1 formandos</span>
+            <span><span class="text-dark def"></span><br>
+            <span><span class="text-dark def"></span><br><br>
+              </p>
+          </div>
+          <center>
+          <span class="text-s f-12 f-t"><b><span class="f-35 f-edoz text-success">- 6% </span></b> de desconto</span><br></center>
+             <!--<a href="pacote.php?p=adm">--><button disabled style="border-radius: 20px!important;" class="btn btn-outline-primary">Assinar pacote</button><!--</a>-->
+            </div>
+        </div>
+    </div>
+              
+        <div class="item">
+          <div class="quadro_curso shadow">
+            <div class="card-footer c2">
+            <div class="curso_conteudo text-start">
+              <h5 class="text-s f-t">SDQ Técnico</h5>
+                <p class="f-13">
+                  <span><span class="text-dark def"><svg version="1.1" xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" fill="green" viewBox="0 0 24 24">
+                  <title>incluido</title>
+                  <path d="M9.331 21.375l-0.346-0.352-0.185-0.183-4.040-4.041 0.002-0.003-3.922-3.922 4.576-4.576 3.917 3.917 9.341-9.341 4.576 4.576-9.869 9.874zM6.872 16.79l2.459 2.459 0.005-0.005 0.003 0.003 2.452-2.452 0.528-0.533 8.811-8.811-2.454-2.454-9.341 9.341-3.917-3.917-2.454 2.454 3.912 3.912z"></path>
+                  </svg> Curso escolhido </span><br>
+                  <span><span class="text-dark def"><svg version="1.1" xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" fill="green" viewBox="0 0 24 24">
+                  <title>incluido</title>
+                  <path d="M9.331 21.375l-0.346-0.352-0.185-0.183-4.040-4.041 0.002-0.003-3.922-3.922 4.576-4.576 3.917 3.917 9.341-9.341 4.576 4.576-9.869 9.874zM6.872 16.79l2.459 2.459 0.005-0.005 0.003 0.003 2.452-2.452 0.528-0.533 8.811-8.811-2.454-2.454-9.341 9.341-3.917-3.917-2.454 2.454 3.912 3.912z"></path>
+                  </svg> Centro escolhido</span><br>
+                  <span><span class="text-dark def"><svg version="1.1" xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" fill="green" viewBox="0 0 24 24">
+                  <title>incluido</title>
+                  <path d="M9.331 21.375l-0.346-0.352-0.185-0.183-4.040-4.041 0.002-0.003-3.922-3.922 4.576-4.576 3.917 3.917 9.341-9.341 4.576 4.576-9.869 9.874zM6.872 16.79l2.459 2.459 0.005-0.005 0.003 0.003 2.452-2.452 0.528-0.533 8.811-8.811-2.454-2.454-9.341 9.341-3.917-3.917-2.454 2.454 3.912 3.912z"></path>
+                  </svg> Certificado</span><br>
+                  <span><span class="text-dark def"><svg version="1.1" xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" fill="green" viewBox="0 0 24 24">
+                  <title>incluido</title>
+                  <path d="M9.331 21.375l-0.346-0.352-0.185-0.183-4.040-4.041 0.002-0.003-3.922-3.922 4.576-4.576 3.917 3.917 9.341-9.341 4.576 4.576-9.869 9.874zM6.872 16.79l2.459 2.459 0.005-0.005 0.003 0.003 2.452-2.452 0.528-0.533 8.811-8.811-2.454-2.454-9.341 9.341-3.917-3.917-2.454 2.454 3.912 3.912z"></path>
+                  </svg> Até 1 formandos</span>
+                  <span><span class="text-dark def"></span><br>
+                  <span><span class="text-dark def"></span><br><br>
+                </p>
+            </div>
+            <center>
+              <span class="text-s f-12 f-t"><b><span class="f-35 f-edoz text-success">- 5% </span></b> de desconto</span><br></center>
+             <!--<a href="pacote.php?p=tec">--><button disabled style="border-radius: 20px!important;" class="btn btn-outline-primary">Assinar pacote</button><!--</a>-->
+          </div>
+          </div>
+      </div>
+    
+    <div class="item">
+      <div class="quadro_curso shadow">
+        <div class="card-footer c3">
+        <div class="curso_conteudo text-start">
+            <h5 class="text-s f-t">SDQ Online</h5>
+            <p class="f-13">
+              <p class="f-13">
+                <span>
+                  <span class="text-dark def">
+                  <svg version="1.1" xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" fill="green" viewBox="0 0 24 24">
+                  <title>incluido</title>
+                  <path d="M9.331 21.375l-0.346-0.352-0.185-0.183-4.040-4.041 0.002-0.003-3.922-3.922 4.576-4.576 3.917 3.917 9.341-9.341 4.576 4.576-9.869 9.874zM6.872 16.79l2.459 2.459 0.005-0.005 0.003 0.003 2.452-2.452 0.528-0.533 8.811-8.811-2.454-2.454-9.341 9.341-3.917-3.917-2.454 2.454 3.912 3.912z"></path>
+                  </svg> Até 15 cursos</span><br>
+                <span><span class="text-dark def">
+                  <svg version="1.1" xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" fill="green" viewBox="0 0 24 24">
+                  <title>incluido</title>
+                  <path d="M9.331 21.375l-0.346-0.352-0.185-0.183-4.040-4.041 0.002-0.003-3.922-3.922 4.576-4.576 3.917 3.917 9.341-9.341 4.576 4.576-9.869 9.874zM6.872 16.79l2.459 2.459 0.005-0.005 0.003 0.003 2.452-2.452 0.528-0.533 8.811-8.811-2.454-2.454-9.341 9.341-3.917-3.917-2.454 2.454 3.912 3.912z"></path>
+                  </svg> Acesso aos 3 centros</span><br>
+                <span><span class="text-dark def">
+                  <svg version="1.1" xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" fill="green" viewBox="0 0 24 24">
+                  <title>incluido</title>
+                  <path d="M9.331 21.375l-0.346-0.352-0.185-0.183-4.040-4.041 0.002-0.003-3.922-3.922 4.576-4.576 3.917 3.917 9.341-9.341 4.576 4.576-9.869 9.874zM6.872 16.79l2.459 2.459 0.005-0.005 0.003 0.003 2.452-2.452 0.528-0.533 8.811-8.811-2.454-2.454-9.341 9.341-3.917-3.917-2.454 2.454 3.912 3.912z"></path>
+                  </svg> Certificado do centro</span><br>
+                <span><span class="text-dark def">
+                  <svg version="1.1" xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" fill="green" viewBox="0 0 24 24">
+                  <title>incluido</title>
+                  <path d="M9.331 21.375l-0.346-0.352-0.185-0.183-4.040-4.041 0.002-0.003-3.922-3.922 4.576-4.576 3.917 3.917 9.341-9.341 4.576 4.576-9.869 9.874zM6.872 16.79l2.459 2.459 0.005-0.005 0.003 0.003 2.452-2.452 0.528-0.533 8.811-8.811-2.454-2.454-9.341 9.341-3.917-3.917-2.454 2.454 3.912 3.912z"></path>
+                  </svg> Até 5 cursos técnicos</span><br>
+                <span><span class="text-dark def">
+                  <svg version="1.1" xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" fill="green" viewBox="0 0 24 24">
+                  <title>incluido</title>
+                  <path d="M9.331 21.375l-0.346-0.352-0.185-0.183-4.040-4.041 0.002-0.003-3.922-3.922 4.576-4.576 3.917 3.917 9.341-9.341 4.576 4.576-9.869 9.874zM6.872 16.79l2.459 2.459 0.005-0.005 0.003 0.003 2.452-2.452 0.528-0.533 8.811-8.811-2.454-2.454-9.341 9.341-3.917-3.917-2.454 2.454 3.912 3.912z"></path>
+                  </svg> até 5 formandos</span><br>
+              </p>
+            </p>
+        </div>
+        <center>
+          <span class="text-s f-12 f-t"><b><span class="f-35 f-edoz text-success">10.000 </span></b> AOA</span><br></center>
+          <a href="pacote.php?p=onl"><button style="border-radius: 20px!important;" class="btn btn-outline-primary">Assinar pacote</button></a>
+          
+        </div>
+      </div>
+  </div>
+  </div>
+    <!--Div onde contem todos apenas os quadros -->
+    </center>
+    <hr>
+
+    
+ <br>
+    <!-- horizontal -->
+<ins class="adsbygoogle"
+     style="display:block"
+     data-ad-client="ca-pub-1921695646246423"
+     data-ad-slot="5121234355"
+     data-ad-format="auto"
+     data-full-width-responsive="true"></ins>
+<script>
+     (adsbygoogle = window.adsbygoogle || []).push({});
+</script>
+    <hr>
+<br>
+
+    <div class="container">
+                <div class="row">
+    <br>
+                    <div class="col-lg-6 col-12 col-md-6 col-sm-12 col-xs-6 container my-1">
+                                            
+                    </div>
+                    
+    </div>
+    
+
+    
+<br>
+<br>
+<br>
+<br>
+<hr>
+    <footer class="foo">
+        <center><span class="text-muted">&copy; 2022 &middot; Se Deus Quiser</span> <br> <span class="text-muted">Powered By <a href="http://gokside.myartsonline.com/">Gokside</a></span> <br></center>
+    </footer>
+    
+
+    <script>
+        
+
+      
+    $('.owl-carousel').owlCarousel({
+    loop:false,
+    margin:30,
+    autoWidth:true,
+    indicators:false,
+    responsiveClass:true,
+    responsive:{
+        0:{
+            items:2,
+            nav:false,
+            loop:false
+        },
+        600:{
+            items:3,
+            nav:false,
+            loop:false
+        },
+        1000:{
+            items:3,
+            nav:false,
+            loop:false
+        }
+    }
+});
+
+    </script>
+</body>
+</html>
